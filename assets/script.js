6d7bf97f5c31c8bc040e29d127fd8bed1794324e
Lauren Ferrett <LFerrett@gmail.com>
2021-12-09 00:21:27 -0500

    Almost working commit of code, still need to trim password to user inputted number.

--- assets/script.js
+++ assets/script.js
@@ -5,16 +5,70 @@ var generateBtn = document.querySelector("#generate");
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var theNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// Prompt the user to enter number of characters the password should be
 var userSize = prompt("How many characters do you want your password to be? (Enter a number between 8 and 128)");
-var userSpecial = confirm("Do you want special characters?");
-var userUpper = confirm("Do you want uppercase letters?");
-var userLower = confirm("Do you want lowercase letters?");
-var userNumber = confirm("Do you want to use numbers?");
 
-var passwordLength = passwordLength = parseInt(userSize);
+// Turns the string inputted by the user into a number
+var passwordLength = parseInt(userSize);
 
+// Creates empty array for password
+var randomPassword = [];
+
+// If else to check to see that the password length the user picked is not 0, less than 8, or more than 128
+if ((isNaN(passwordLength) === false) || (passwordLength > 8) || (passwordLength < 128)) {
+  var userSpecial = confirm("Do you want to include special characters?");
+  var userUpper = confirm("Do you want to include uppercase letters?");
+  var userLower = confirm("Do you want to include lowercase letters?");
+  var userNumber = confirm("Do you want to include numbers?");
+} else {
+  alert("The password needs to contain between 8 and 128 characters, please try again!");
+}
+
+// Run a funtion to take user inputs and add them to the randomPassword Array
+function generatePassword() {
+  if (userSpecial == true) {
+    randomPassword = randomPassword.concat(specialCharacters);
+    console.log(randomPassword);
+  }
+  if (userUpper == true) {
+    randomPassword = randomPassword.concat(upperCase);
+    console.log(randomPassword);
+  }
+  if (userLower == true) {
+    randomPassword = randomPassword.concat(lowerCase);
+    console.log(randomPassword);
+  }
+  if (userNumber == true) {
+    randomPassword = randomPassword.concat(theNumbers);
+    console.log(randomPassword);
+  }
+} 
+generatePassword();
+
+function finalPassword() {
+  for (var i = 0; i < passwordLength.length; i++) {
+    var cleanPassword = randomPassword[Math.floor(Math.random() * passwordLength.length)];
+    console.log(cleanPassword);
+   }
+}
+finalPassword();
+
+// Function that writes password to the HTML code for copying
+function writePassword() {
+  var password = randomPassword;
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+writePassword();
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// TRASH CODE GOES HERE
 // Prompt the user to enter input and capture them in variables 
 // function promptUser() {
 //   var userSize = prompt("How many characters do you want your password to be? (Enter a number between 8 and 128)");
@@ -34,20 +88,3 @@ var passwordLength = passwordLength = parseInt(userSize);
 //   }
 // }
 
-// Create password with user inputs
-
-function generatePassword() {
-console.log(passwordLength, userSpecial, userUpper, userLower, userNumber)
-}
-generatePassword();
-
-// Write password to the #password input
-function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
